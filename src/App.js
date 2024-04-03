bce0cadd104555efd9d0d75242ae54d1432ccc1a
ayodeji <ayodejiakintobi1@gmail.com>
2024-04-03 11:43:56 +0100

    CREATE: FOLDER

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { FiSettings } from 'react-icons'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import './App.css'
 
 const App = () => {
     return (
-        <div className='underline text-5xl'>
-            App
+        <div className=''>
+            <BrowserRouter>
+                <div className='flex relative dark:main-dark-bg'>
+                    <div className=''>
+
+                    </div>
+                </div>
+            </BrowserRouter>
         </div>
     )
 }
