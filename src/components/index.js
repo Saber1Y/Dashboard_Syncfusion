adec56cee7f4ebc6dada8ca947a4ef99888a36a0
ayodeji <ayodejiakintobi1@gmail.com>
2024-04-18 09:37:47 +0100

    CREATE: COMP CHARTS

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+export { defualt as LineChart } from './Charts/LineChart';
+export { defualt as Pie } from './Charts/Pie';
+export { defualt as SparkLine } from './Charts/SparkLine';
+export { defualt as Stacked } from './Charts/Stacked';
+
+export { defualt as Cart } from './Cart'
+export { defualt as Button } from './Button';
+export { defualt as Footer } from './Footer'
+export { defualt as Header } from './Header'
+export { defualt as Navbar } from './Navbar'
+export { defualt as Themesettings } from './Themesettings'
+export { defualt as Charts } from './Charts'
+export { defualt as Chat } from './Chat'
+
