e8e4041c0b2ad80281cbce82fc4342d5d1e61561
ayodeji <ayodejiakintobi1@gmail.com>
2024-05-02 16:43:24 +0100

    UPDATE: CONTEXT ADDED

---
+++ src/context/ContextProvider.js
@@ -0,0 +1,10 @@
+import React, { CreateContext, UseContext, UseContext, UseState } from 'react'
+
+
+const StateContext = CreateContext();
+
+const initialState = {
+    notification: false,
+    cart: false,
+    chat: false
+}
