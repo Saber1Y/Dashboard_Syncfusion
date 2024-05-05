b270ce7315f21740d878d76d3ac784ac8a23b841
ayodeji <ayodejiakintobi1@gmail.com>
2024-05-05 21:45:01 +0100

    UPDATE: NAVBAR

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,35 @@
 import React, { useEffect }from 'react'
-import { AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineMenu } from 'react-icons/ai';
+import { FiShoppingCart } from 'react-icons/fi';
+import { BsChat } from 'react-icons/bs';
+import { RiNotifaction3Line } from 'react-icons/ri';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import avatar from '../data/avatar.jpg';
+import { Button, Cart, Chat, Notification, UserProfile } from '.';
+import { useStateContext } from '../context/ContextProvider';
 
+
+const NavButton = ({ title, customfunc, icon, dotColor }) => {
+  <TooltipComponent
+   content={title}
+   position='BottomCenter'
+  >
+    <Button type="button" onClick={customfunc} style={{ color }} className="relative text-xl rounded-full p-3 hover:bg-light-gray" >
+      <span style={{ background:dotColor }} className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2' >
+        {icon}
+      </span>
+    </Button>
+  </TooltipComponent>
+}
 
 const Navbar = () => {
-  return (
-    <div>
 
+  const { activeMenu, setActiveMenu } = useStateContext();
+
+  return (
+    <div className='flex justify-between p-2 md:mx-6 relative'>
+      <NavButton title='menu' customfunc={setActiveMenu((prevActiveMenu) => !prevActiveMenu) } />
     </div>
   )
 }
