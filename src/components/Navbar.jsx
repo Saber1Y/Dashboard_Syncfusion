d95a3d56a34783a45af5cb1a369c4944f2a23714
ayodeji <ayodejiakintobi1@gmail.com>
2024-05-07 17:42:35 +0100

    UPDATE : NAVBTN DISPLAYED

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,18 +10,22 @@ import { Button, Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../context/ContextProvider';
 
 
-const NavButton = ({ title, customfunc, icon, dotColor }) => {
-  <TooltipComponent
-   content={title}
-   position='BottomCenter'
-  >
-    <Button type="button" onClick={customfunc} style={{ color }} className="relative text-xl rounded-full p-3 hover:bg-light-gray" >
-      <span style={{ background: dotColor }} className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2' >
-        {icon}
-      </span>
-    </Button>
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
+  <TooltipComponent content={title} position="BottomCenter">
+    <button
+      type="button"
+      onClick={() => customFunc()}
+      style={{ color }}
+      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+    >
+      <span
+        style={{ background: dotColor }}
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+      />
+      {icon}
+    </button>
   </TooltipComponent>
-}
+);
 
 const Navbar = () => {
 
