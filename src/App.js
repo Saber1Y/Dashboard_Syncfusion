4e4a93bcb718991bfd1b2a892a26e279d842e7a7
ayodeji <ayodejiakintobi1@gmail.com>
2024-05-11 15:20:47 +0100

    UPDATE

--- src/App.js
+++ src/App.js
@@ -1,71 +1,91 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CiSettings } from "react-icons/ci";
-import { TooltipComponent } from '@syncfusion/ej2-react-popups'
-import { Navbar, Footer, Themesettings, Sidebar } from './components/index';
-import { Orders, Stacked, Pyramid, Area, Financial, Pie, Bar } from './pages/index';
-import { useStateContext } from './context/ContextProvider';
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { Navbar, Footer, Themesettings, Sidebar } from "./components/index";
+import {
+  Orders,
+  Stacked,
+  Pyramid,
+  Area,
+  Financial,
+  Pie,
+  Bar,
+} from "./pages/index";
+import { useStateContext } from "./context/ContextProvider";
 
-import './App.css'
+import "./App.css";
+import Ecommerce from "./pages/Ecommerce";
 
 const App = () => {
+  const { activeMenu } = useStateContext();
 
-    const { activeMenu } = useStateContext();
-
-    return (
-        <div className=''>
-            <BrowserRouter>
-                <div className='flex relative dark:main-dark-bg'>
-                    <div className='fixed right-4 bottom-4'>
-                        <TooltipComponent content="settings" position="Top" style={{ zIndex: '1000' }}>
-                            <button type='button' className='text-4xl p-3 hover:bg-shadow-xl hover:bg-light-gray text-white' style={{ background: 'black', borderRadius: '50px' }}>
-                                <CiSettings />
-                            </button>
-                        </TooltipComponent>
-                    </div>
-                </div>
-                {
-                    activeMenu ? (
-                        <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+  return (
+    <div className="">
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                onClick={() => {}}
+                // style={{ background:  borderRadius: '50%' }}
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+              >
+                <CiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
               <Sidebar />
             </div>
           ) : (
             <div className="w-0 dark:bg-secondary-dark-bg">
               <Sidebar />
             </div>
-                    )
-                }
+          )}
+          <div
+            className={
+              activeMenu
+                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+            }
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              <Navbar />
+            </div>
+            <div>
+              <Routes>
+                {/* dashboard  */}
+                <Route path="/" element={<Ecommerce />} />
+                <Route path="/ecommerce" element={<Ecommerce />} />
 
-                <div className={
-                    `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-1'}`
-                }>
-                    <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-                        <Navbar />
-                    </div>
+                {/* <Route path="/orders" element={<Orders />} />
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/customers" element={<Customers />} /> */}
 
-                    <div>
-                        <Routes>
-                            <Route path='/' element="Ecommerce" />
-                            <Route path='/ecommerce' element="Ecommerce" />
+                {/* <Route path="/kanban" element={<Kanban />} />
+                <Route path="/editor" element={<Editor />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/color-picker" element={<ColorPicker />} /> */}
 
-                            <Route path='orders' element="Orders" />
-                            <Route path='employees' element="Employees" />
-                            <Route path='customers' element="Customers" />
-
-                            <Route path="/line" element="line" />
-                            <Route path="/area" element="Area" />
-                            <Route path="/bar" element="Bar" />
-                            <Route path="/pie" element="Pie" />
-                            <Route path="/financial" element="Financial" />
-                            {/* <Route path="/color-mapping" element="line" /> */}
-                            <Route path="/pyramid" element="Pyramid" />
-                            <Route path="/stacked" element="Stacked" />
-                        </Routes>
-                    </div>
-                </div>
-            </BrowserRouter>
+                {/* <Route path="/line" element={<Line />} />
+                <Route path="/area" element={<Area />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/financial" element={<Financial />} />
+                <Route path="/color-mapping" element={<ColorMapping />} />
+                <Route path="/pyramid" element={<Pyramid />} />
+                <Route path="/stacked" element={<Stacked />} /> */}
+              </Routes>
+            </div>
+            {/* <Footer /> */}
+          </div>
         </div>
-    )
-}
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
