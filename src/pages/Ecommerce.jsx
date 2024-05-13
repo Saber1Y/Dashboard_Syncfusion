0cb9a865a7802db495aff5681be6fc9b8df6c6a7
ayodeji <ayodejiakintobi1@gmail.com>
2024-05-13 19:39:20 +0100

    UPDATE:: BTN DISPLAYED

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,18 +1,37 @@
-import React, {useEffect, useState} from 'react'
-import { BsCurrencyDollar } from 'react-icons/bs';
-import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
-import { useStateContext } from '../context/ContextProvider';
+import React, { useEffect, useState } from "react";
+import { BsCurrencyDollar } from "react-icons/bs";
+import {
+  earningData,
+  SparklineAreaData,
+  ecomPieChartData,
+} from "../data/dummy";
+import { useStateContext } from "../context/ContextProvider";
+import { Button } from "../components";
 
 const Ecommerce = () => {
   return (
-    <div className='mt-12'>
-      <div className='flex flex-wrap lg:flex-nowrap justify-center'>
-        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-75 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center'>
-
+    <div className="mt-12">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-75 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+          <div className="flex justify-between items-center">
+            <div>
+              <p className="text-gray-200 text-base">Ecommerce</p>
+              <p className="text-2xl text-black-500">$50,361</p>
+            </div>
+          </div>
+          <div className="mt-6">
+            <Button
+              color="white"
+              bgColor="blue"
+              text="Download"
+              borderRadius="10px"
+              size="md"
+            />
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Ecommerce
+export default Ecommerce;
