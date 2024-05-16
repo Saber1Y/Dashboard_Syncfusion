9218f9aef2f3763341520c054a0fe7ce63796e2f
ayodeji <ayodejiakintobi1@gmail.com>
2024-05-16 23:29:05 +0100

    CREATE: Sparkline comp

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -9,6 +9,9 @@ import {
 import { useStateContext } from "../context/ContextProvider";
 import { Button } from "../components";
 import { data } from "autoprefixer";
+import { SparkLine } from '../components'
+
+
 
 const Ecommerce = () => {
   return (
@@ -59,28 +62,63 @@ const Ecommerce = () => {
       </div>
 
       <div className="flex flex-wrap justify-between">
-      <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
           <div className="flex justify-between">
-        <p className="font-semibold text-xl">Revenue Updates</p>
-        <div className="flex items-center gap-4">
-          <p className="flex items-center gap-2 text-gray-200 hover:drop-shadow-xl9">
-            <span>
-              <LuDot />
-            </span>
-            <span>Expense</span>
-          </p>
-          <p className="flex items-center gap-2 text-gray-400 hover:drop-shadow-xl">
-          <span>
-              <LuDot />
-            </span>
-            <span>Budget</span>
-          </p>
+            <p className="font-semibold text-xl">Revenue Updates</p>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-200 hover:drop-shadow-xl9">
+                <span>
+                  <LuDot />
+                </span>
+                <span>Expense</span>
+              </p>
+              <p className="flex items-center gap-2 text-gray-400 hover:drop-shadow-xl">
+                <span>
+                  <LuDot />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            <div className=" border-r-1 border-color m-4 pr-10">
+              <div>
+                <p>
+                  <span className="text-3xl font-semibold">$93,438</span>
+                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                    23%
+                  </span>
+                </p>
+                <p className="text-gray-500 mt-1">Budget</p>
+              </div>
+              <div className="mt-8">
+                <p className="text-3xl font-semibold">$48,487</p>
+
+                <p className="text-gray-500 mt-1">Expense</p>
+              </div>
+
+              <div className="mt-5">
+                <SparkLine id="line-sparkLine" type="Line" height="80px" width="250px" data={SparklineAreaData}  />
+              </div>
+              <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor={''}
+                  text="Download Report"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+            <div>
+              {/* <Stacked  width="320px" height="360px" /> */}
+            </div>
+          </div>
+        </div>
+
         </div>
       </div>
-      </div>
 
-    </div>
-    </div>
   );
 };
 
