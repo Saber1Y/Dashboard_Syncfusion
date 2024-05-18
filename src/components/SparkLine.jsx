3894c0ec6d4eb3329e1447570990211704ff5f14
ayodeji <ayodejiakintobi1@gmail.com>
2024-05-18 23:24:51 +0100

    UPDATE: CHART

--- src/components/SparkLine.jsx
+++ src/components/SparkLine.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import {
-  SparklineComponent,
-  inject,
-  SparklineTooltip,
-} from "@syncfusion/ej2-react-charts";
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
 const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
   return (
-    <SparklineComponent>
-        
+    <SparklineComponent id={id} height={height} width={width} lineWidth={1} valueType="Numeric" fill={color} border={{ color: currentColor, width: width, }} dataSource={data} xName="x" yName="y" type={type} >
+        <Inject services={[SparklineTooltip]} />
     </SparklineComponent>
   )
 };
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -98,7 +98,14 @@ const Ecommerce = () => {
               </div>
 
               <div className="mt-5">
-                {/* <SparkLine id="line-sparkLine" type="Line" height="80px" width="250px" data={SparklineAreaData} color={currentColor} /> */}
+                <SparkLine
+                  id="line-sparkLine"
+                  type="Line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color={currentColor}
+                />
               </div>
 
               <div className="mt-10">
@@ -117,7 +124,7 @@ const Ecommerce = () => {
       </div>
 
       <div>
-        <div className=" rounded-2xl md:w-400 p-4 m-3" >
+        <div className=" rounded-2xl md:w-400 p-4 m-3">
           <div className="flex justify-between items-center ">
             <p className="font-semibold text-white text-2xl">Earnings</p>
 
@@ -130,24 +137,22 @@ const Ecommerce = () => {
           </div>
 
           <div className="mt-4">
-              {/* <SparkLine currentColor={currentColor} id="column-sparkLine" height="100px" type="Column" data={SparklineAreaData} width="320" color="rgb(242, 252, 253)" /> */}
-            </div>
+            {/* <SparkLine currentColor={currentColor} id="column-sparkLine" height="100px" type="Column" data={SparklineAreaData} width="320" color="rgb(242, 252, 253)" /> */}
+          </div>
         </div>
 
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl md:w-400 p-8 m-3 flex justify-center items-center gap-10">
-        <div>
-              <p className="text-2xl font-semibold ">$43,246</p>
-              <p className="text-gray-400">Yearly sales</p>
-            </div>
+          <div>
+            <p className="text-2xl font-semibold ">$43,246</p>
+            <p className="text-gray-400">Yearly sales</p>
+          </div>
 
-            
-            <div className="w-40">
-              {/* <Pie id="pie-chart" data={ecomPieChartData} legendVisiblity={false} height="160px" /> */}
-            </div>
+          <div className="w-40">
+            {/* <Pie id="pie-chart" data={ecomPieChartData} legendVisiblity={false} height="160px" /> */}
+          </div>
         </div>
       </div>
     </div>
-    
   );
 };
 
