627258d4513f79397c642575794a0149c6cd327c
ayodeji <ayodejiakintobi1@gmail.com>
2024-05-31 21:52:36 +0100

    CREATE: Customers

---
+++ src/pages/Customers.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Selection,
+  Inject,
+  Edit,
+  Toolbar,
+  Sort,
+  Filter,
+} from "@syncfusion/ej2-react-grids";
+
+import { customersData, customersGrid } from "../data/dummy";
+
+import { Header } from "../components";
+
+const Customers = () => {
+  return (
+    <div className="m-2 md:m-10 p-2 md-p-10 bg-white rounded-3xl">
+      <Header title="Customers " />
+      <GridComponent
+        id="gridcomp"
+        dataSource={employeesData}
+        allowSorting
+        allowPaging
+        width="auto"
+      >
+        <ColumnsDirective>
+          <Inject services={[]} />
+        </ColumnsDirective>
+      </GridComponent>
+    </div>
+  );
+};
+
+export default Customers;
