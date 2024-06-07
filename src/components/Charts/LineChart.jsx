5f0552501d7de2a2904312897715dfa56723fe4f
ayodeji <ayodejiakintobi1@gmail.com>
2024-06-07 21:15:44 +0100

    UPDATE: LINE-CHART

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,8 +1,38 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  Tooltip,
+  LineSeries,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  lineCustomSeries,
+  LinePrimaryYAxis,
+  LinePrimaryXAxis,
+} from "../../data/dummy";
 
 const LineChart = () => {
   return (
-  <div>LineChart</div>
+    <ChartComponent
+      id="lineChart"
+      height="420px"
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      tooltip={{ enabled: true }}
+      chartArea={{ border: { width: 0 } }}
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((series, index) => (
+          <SeriesDirective key={index} {...series} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   );
 };
 
